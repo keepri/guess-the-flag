2850ec7f88c3ec78e52118ea3a2e8e7913fddd31
KIPRI <hi@kipri.dev>
2023-06-05 18:09:42 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ciprian-Liviu Pîțul on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
